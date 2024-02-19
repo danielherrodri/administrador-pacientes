a0e8d191fd79e947e47ce1e55285819b80c719ab
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-02-18 22:59:24 -0600

    Vídeo 76 - Finalizando el Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -17,6 +17,56 @@
           class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
         />
       </div>
+      <div class="mb-5">
+        <label
+          for="propietario"
+          class="block text-gray-700 uppercase font-bold"
+        >
+          Nombre Propietario
+        </label>
+        <input
+          id="propietario"
+          type="text"
+          placeholder="Nombre del propietario"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="email" class="block text-gray-700 uppercase font-bold">
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Email del propietario"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="alta" class="block text-gray-700 uppercase font-bold">
+          Alta
+        </label>
+        <input
+          id="alta"
+          type="date"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+      <div class="mb-5">
+        <label for="sintomas" class="block text-gray-700 uppercase font-bold">
+          Síntomas
+        </label>
+        <textarea
+          id="sintomas"
+          placeholder="Describe los síntomas del paciente"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+        />
+      </div>
+      <input
+        type="submit"
+        class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+        value="Registrar Paciente"
+      />
     </form>
   </div>
 </template>
