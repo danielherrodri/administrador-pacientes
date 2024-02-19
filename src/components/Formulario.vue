8831546bd866d04e642f1f374e735e03799f27d0
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-02-18 22:53:40 -0600

    Vídeo 75 - Primeros pasos con el Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,9 +2,22 @@
   <div class="md:w-1/2">
     <h2 class="font-black text-3xl text-center">Seguimiento pacientes</h2>
     <p class="text-lg mt-5 text-center mb-10">
-        Añade pacientes
-        <span class="text-indigo-600 font-bold">Adminístralos</span>
+      Añade pacientes
+      <span class="text-indigo-600 font-bold">Adminístralos</span>
     </p>
+    <form action="" class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <div class="mb-5">
+        <label for="mascota" class="block text-gray-700 uppercase font-bold">
+          Nombre Mascota
+        </label>
+        <input
+          id="mascota"
+          type="text"
+          placeholder="Nombre de la mascota"
+          class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+        />
+      </div>
+    </form>
   </div>
 </template>
 <script setup></script>
