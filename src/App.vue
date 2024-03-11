bfe7a9696926b22b15d925a13f6ded770f48da62
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-03-10 23:34:13 -0600

    Vídeo 91 - Mostrando los pacientes agregados

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, reactive } from "vue";
 import Header from "./components/Header.vue";
 import Formulario from "./components/Formulario.vue";
+import Paciente from "./components/Paciente.vue";
 
 const pacientes = ref([]);
 const paciente = reactive({
@@ -33,7 +34,13 @@ const guardarPaciente = () => {
         <h3 class="font-black text-3xl text-center">
           Administra tus Pacientes
         </h3>
-        <div v-if="pacientes.length > 0"></div>
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de
+            <span class="text-indigo-600 font-bold">Pacientes</span>
+          </p>
+          <Paciente for="paciente in pacientes" :paciente="paciente" />
+        </div>
         <p v-else class="mt-10 text-2xl text-center">No hay pacientes</p>
       </div>
     </div>
